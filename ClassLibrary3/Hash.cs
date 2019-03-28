4e9dd1c882af8ef1089de04381a21d4d740fab50
Никита Паренюк <parenyuk2014@gmail.com>
2019-03-28 20:33:23 +0500

    I'm done

---
+++ ClassLibrary3/Hash.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Hash
+{
+    public class Element
+    {
+        public object Key { get; private set; }
+        public object Value { get; private set; }
+
+        public Element (object key, object value)
+        {
+            Key = key;
+            Value = value;
+            if (Key is null)
+            {
+                throw new ArgumentNullException (nameof (key));
+            }
+
+            if (Value is null)
+            {
+                throw new ArgumentNullException (nameof (value));
+            }
+        }
+    }
+    class HashTable
+    {
+        readonly int Size;
+        readonly Element[] array;
+       
+        public HashTable (int size)
+        {
+            array = new Element[size];
+            Size = size;
+        }
+
+        public void PutPair (object key, object value)
+        {
+            var hash = Math.Abs(key.GetHashCode());
+            var indexof = hash % Size;  
+            for (; array[indexof] != null; indexof = (indexof + 1) % Size)
+                if (array[indexof].Key.Equals(key))
+                    break;
+            array[indexof] = new Element(key, value);
+        }
+
+        public object GetValueByKey (object key)
+        {
+            foreach (var e in array)
+            {
+                if (e.Key.Equals(key))
+                    return e.Value;
+            }
+            return null;
+        }
+    }
+}
---
+++ ClassLibrary3/Tests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using Hash;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1 ()
+        {
+            string firstName = "Frog", secondName = "Eagle", thirdName = "Pizza";
+            var hashTable = new HashTable(3);
+            hashTable.PutPair (0, firstName);
+            hashTable.PutPair (1, secondName);
+            hashTable.PutPair (2, thirdName);
+            Assert.AreEqual (firstName, hashTable.GetValueByKey (0));
+            Assert.AreEqual (secondName, hashTable.GetValueByKey (1));
+            Assert.AreEqual (thirdName, hashTable.GetValueByKey (2));
+        }
+
+        [Test]
+        public void Test2 ()
+        {
+            var hashTable = new HashTable (2);
+            hashTable.PutPair (2, 1);
+            hashTable.PutPair (2, 3);
+            Assert.AreEqual (3, hashTable.GetValueByKey (2));
+        }
+
+        [Test]
+        public void Test3 ()
+        {
+            var hashTable = new HashTable (10000);
+            for (var i = 0; i < 10000; i++)
+            {
+                hashTable.PutPair ((i * 1000), i);
+            }
+            Assert.AreEqual (9, hashTable.GetValueByKey (9000));
+        }
+
+        [Test]
+        public void Test4 ()
+        {
+            var hashTable = new HashTable(10000);
+            for (int i = 0; i < 10000; i++)
+            {
+                hashTable.PutPair(i, i * 1000);
+            }
+            for (int i = 10000; i < 11000; i++)
+            {
+                Assert.AreEqual(hashTable.GetValueByKey(i), null);
+            }
+        }
+    }
+}
